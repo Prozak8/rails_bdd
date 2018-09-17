eefab2391fac5f02c98f9b0f6f0ef9dc064e7ae2
Noel Ryan <noelgerard.ryan@gmail.com>
2018-09-17 15:52:55 +0200

    set up rspec and shoulda-matchers

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,13 @@ module RailsDemo
     # the framework and any gems in your application.
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+      generate.helper_specs false
+      generate.routing_specs false
+      generate.controller_specs false
+      generate.system_tests false
   end
 end
