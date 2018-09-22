2d4c0aca5917cf1e2eeebae003548b66ac40d13d
Zak <zakary.howard94@gmail.com>
2018-09-22 08:57:41 +0200

    added edit and show button, index now displays content in columns

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,10 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
  
   def create
     @article = Article.new(article_params)
@@ -14,8 +18,25 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+   
+    redirect_to @article
   end
 
   private
